0e8c136f0e1612f465d507c1a3fb4ef004416ffe
fabio-elioterio <fabioelioterio.77@gmail.com>
2024-12-04 16:37:40 -0300

    feat: create contact model

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Contact(models.Model):
+
+    # models.someField() define qual o tipo do campo/atributo
+    firt_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50, blank=True) # blank = True informa que o campo é opcional.
+    phone = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254, blank=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank=True)
